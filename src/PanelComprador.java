b10ea3a9b14cc811e93d995b38489ddf4e00deb0
Jsilva221 <juasilva2021@inf.udec.cl>
2023-05-13 17:16:40 -0400

    Tarea 1

---
+++ src/PanelComprador.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class PanelComprador extends JPanel {
+    public void paint (Graphics g){
+        super.paint(g);
+    }
+}
---
+++ src/PanelExpendedor.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelExpendedor extends JPanel {
+    public void paint (Graphics g){
+        super.paint(g);
+    }
+}
