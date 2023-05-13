76b8aaf5bc8efb125fff895c5b92d2f809565fff
Jsilva221 <juasilva2021@inf.udec.cl>
2023-05-13 17:32:27 -0400

    Tarea 1

--- src/PanelPrincipal.java
+++ src/PanelPrincipal.java
@@ -11,7 +11,7 @@ public class PanelPrincipal extends JPanel {//se ve en el centro de la ventana
     public void paint (Graphics g) { //todo se dibuja a partir de este método
         super.paint(g); //llama al método pint al que hace override en la superclase
                         //el de la super clase solo pinta el fondo (background)
-        com.paint(g); //llama al metodo paint definido en el PanelComprador
-        exp.paint(g); //llama al metodo paint definido en el PanelExpendedor
+        com.paint(g);   //llama al metodo paint definido en el PanelComprador
+        exp.paint(g);   //llama al metodo paint definido en el PanelExpendedor
     }
 }
